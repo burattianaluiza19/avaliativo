ab8b91af59c164bf089e6a9eda7166c0b8992313
burattianaluiza19 <ana.buratti.barbosa@escola.pr.gov.br>
2026-05-20 12:12:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,6 +15,53 @@ async function sasa() {
         
     }
 
+        pessoas.forEach(item => {
+            const linha = document.createElement('tr');
+
+            //convrt objeto para string no onclick 
+            const itemString = JSON.stringify(item).replaceAll('"', '&quot;');
+
+            linha.innerHTML = ` 
+                <td>${item.nome}</td>
+                <td>${item.iade}</td>
+                <td>${item.ano}</td>
+                <td>${item.habiliade}</td>
+                <td>
+                   <button type= "button" onclick= "claros (${itemString})">Slecionar</button>
+                   <type ="button" onclick="thi.colset('tr').remove()">Fds</button>
+                </td>
+                `;
+
+                 destino.appendChild(linha);
+        });
+
+      } catch (error) {  
+        console.error("Error ao buscar dados:", error);
+
+         } 
+    { 
+
+    function blex(){ 
+        const destino = document.getElementById('destino');
+        destino.innerHTML = '';
+
+    }
+
+    function claros(objetopesoa) {
+    //captura os inputs do HTML pelos 
+    document.getElementById('nome').value = objetopessoa.nome;
+    document.getElementById('idade').value = objetopessoa.idade;
+    document.getElementById('ano').value = objetopessoa.ano;
+    document.getElementById('hablidade').value = objetopessoa.habiliade;
+
+    }
+
+                
+
+
+
+
+
 }
 
 
